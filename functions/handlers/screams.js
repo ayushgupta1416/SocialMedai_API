96ff83020a6d8e0bd9ed9f3e576726453d09977c
ayushgupta1416 <ayushgupta1416@gmail.com>
2020-01-25 19:40:01 +0530

    added some functionality

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -33,19 +33,127 @@ exports.postOneScream=(req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),
+        likeCount: 0,
+        commentCount: 0
     };
-
+console.log(newScream);
     db
         .collection('screams')
         .add(newScream)
         .then(doc => {
+          const resScream=newScream;
+          resScream.screamId=doc.id();
 
-            res.json({ message: `document ${doc.id} created succesfully` });
+            return res.json(resScream);
         })
         .catch(err => {
-            res.status(500).json({ error: `Something went wrong` });
+            
             console.log(err);
+            return res.status(500).json({ error: err.code });
         })
 
 };
+exports.getScream= (req, res) => {
+  console.log("yoo");
+    let screamData = {};
+    db.doc(`/screams/${req.params.screamId}`)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          return res.status(404).json({ error: 'Scream not found' });
+        }
+        screamData = doc.data();
+        screamData.screamId = doc.id;
+        return db
+          .collection('comments')
+          .orderBy('createdAt', 'desc')
+          .where('screamId', '==', req.params.screamId)
+          .get();
+      })
+      .then((data) => {
+        screamData.comments = [];
+        data.forEach((doc) => {
+          screamData.comments.push(doc.data());
+        });
+         return res.json(screamData);
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  };
+
+  exports.postComment=(req,res)=>{
+    const newComment={
+      body:req.body.body,
+      createdAt:new Date().toISOString(),
+      screamId:req.params.screamId,
+      userHandle:req.user.handle,
+      userImage:req.user.imageUrl
+      };
+    
+      db.doc(`/screams/${req.params.screamId}`)
+      .get()
+      .then(doc=>{
+        if(!doc.exists){
+          return res.json("scream not found");
+        }
+        return db.collection('comments').add(newComment)
+      })
+      .then(()=>{
+         res.json(newComment);
+      })
+      .catch(err=>{
+        console.error(err);
+         return res.status(404).json({err:"something went wrong"});
+      });
+        
+  };
+  exports.likeScream = (req, res) => {
+    const likeDocument = db
+      .collection('likes')
+      .where('userHandle', '==', req.user.handle)
+      .where('screamId', '==', req.params.screamId)
+      .limit(1);
+  
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`);
+  
+    let screamData;
+  
+    screamDocument
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          screamData = doc.data();
+          screamData.screamId = doc.id;
+          return likeDocument.get();
+        } else {
+          return res.status(404).json({ error: 'Scream not found' });
+        }
+      })
+      .then((data) => {
+        if (data.empty) {
+          return db
+            .collection('likes')
+            .add({
+              screamId: req.params.screamId,
+              userHandle: req.user.handle
+            })
+            .then(() => {
+              screamData.likeCount++;
+              return screamDocument.update({ likeCount: screamData.likeCount });
+            })
+            .then(() => {
+              return res.json(screamData);
+            });
+        } else {
+          return res.status(400).json({ error: 'Scream already liked' });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  };
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./utility/fbAuth');
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream,getScream ,postComment,likeScream} = require('./handlers/screams');
 const { signup, login,uploadImage,addUserDetails,getUserDetails } = require('./handlers/users');
 
 
@@ -11,9 +11,12 @@ app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 
 //user routes
+app.get('/scream/:screamId', getScream);
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('user/image',FBAuth ,uploadImage);
+app.post('/user/image',FBAuth ,uploadImage);
 app.post('/user',FBAuth,addUserDetails);
 app.get('/user',FBAuth,getUserDetails);
+app.post('/scream/:screamId/comment',FBAuth,postComment);
+app.get('/scream/:screamId/like',FBAuth,likeScream)
 exports.api = functions.https.onRequest(app);
